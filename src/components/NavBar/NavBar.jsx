3bb6ff3f6f87ab9a89bdc70bd2581ed3c9a9e94f
Carlos Daniel <caduccino@live.com>
2021-12-11 16:24:01 -0300

    Features atualizadas

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import Gear from '../../images/settings.png'
 
 import './navbar.css';
 
@@ -11,34 +12,51 @@ const NavBar = ({ ...props }) => {
   const theNumber = props.roteiros;
 
   return (
-    <nav className="py-3">
-      <div className="container">
-        <div className="row d-flex justify-content-between">
-          <div className="col-12 d-flex justify-content-between align-items-center">
-            <div className="d-flex align-items-center">
-              <div className="user-picture" />
-              <div className="user-info">
-                <span className="name">{ theName }</span>
-                <br />
-                {' '}{ theNumber }{' '}roteiros
+    <div>
+      <nav className="py-3">
+        <div className="container">
+          <div className="row d-flex justify-content-between">
+            <div className="col-12 d-flex justify-content-between align-items-center">
+              <Link to="/my-travels" className="home-link">
+                <div className="d-flex align-items-center">
+                  <div className="user-picture" />
+                  <div className="user-info">
+                    <span className="name">{ theName }</span>
+                    <br />
+                    {' '}{ theNumber }{' '}roteiros
+                  </div>
+                </div>
+              </Link>
+              <div>
+                <Link to="/" className="logout">
+                  <Button>
+                    logout
+                  </Button>
+                </Link>
+                <Link to="/social" className="hide">
+                  <Button variant="outline-secondary ms-3">
+                    Social
+                  </Button>
+                </Link>
+                <img src={ Gear } alt="Settings Icon" className="gear hide" />
               </div>
-            </div>
-            <div>
-              <Link to="/" className="logout">
-                <Button>
-                  logout
-                </Button>
-              </Link>
-              <Link to="/social" className="logout">
-                <Button variant="outline-secondary ms-3">
-                  Social
-                </Button>
-              </Link>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <section id="footer" className="show">
+        <Link to="/social">
+          <Button variant="outline-secondary ms-3">
+            Social
+          </Button>
+        </Link>
+        <Link to="/social">
+          <Button variant="outline-secondary ms-3">
+            Ajustes
+          </Button>
+        </Link>
+      </section>
+    </div>
   );
 };
 
